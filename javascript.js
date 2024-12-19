e575fa990cec4377f9500ea03347998677057d6a
danielalagerstrom <eaa24dcl@students.eaaa.dk>
2024-12-19 09:24:24 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -34,7 +34,7 @@ const now = new Date().getHours();
 
 if (document.body.id === 'home') {  // Check if we are on the front page
   if (now >= 6 && now < 12) {
-    hero.style.backgroundImage = "url(Images/morningsauna.jpg')";
+    hero.style.backgroundImage = "url(Images/morningsauna.jpg)";
     hero.querySelector('h2').textContent = "Start your day with sauna bliss!";
   } else if (now >= 12 && now < 18) {
     hero.style.backgroundImage = "url('..Images/noon_sauna.png')";
